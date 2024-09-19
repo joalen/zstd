dd04701f518f34381ea0a5c54b25650c42103b1d
Alen Jo <45494246+joalen@users.noreply.github.com>
2024-09-18 23:33:59 -0500

    Update fileio.c

--- programs/fileio.c
+++ programs/fileio.c
@@ -1139,7 +1139,7 @@ static cRess_t FIO_createCResources(FIO_prefs_t* const prefs,
 
     //ress.writeCtx = AIO_WritePool_create(prefs, ZSTD_CStreamOutSize());
     ress.writeCtx = (WritePoolCtx_t*) malloc(sizeof(WritePoolCtx_t)); // assertion is solved #1
-    &ress.writeCtx->base->availableJobsCount = 2 // assertion is solved #2
+    ress.writeCtx->base->availableJobsCount = 2 // assertion is solved #2
     ress.readCtx = AIO_ReadPool_create(prefs, ZSTD_CStreamInSize());
 
     /* Advanced parameters, including dictionary */
